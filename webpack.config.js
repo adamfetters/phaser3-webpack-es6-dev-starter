df6c3d6d3ff61a03bea448a7bf7f8db20fec860c
Adam Fetters <adamfetterscoder@gmail.com>
2019-06-01 09:14:34 -0400

    Added rules for webpack to use babel-loader on

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,19 @@ module.exports = {
 		path: path.resolve(__dirname, 'build'),
 		filename: 'app.bundle.js',
 	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src/'),
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env'],
+					},
+				},
+			},
+		],
+	},
 };
